c336b202e0c16179b391dbd85e19e18f1ce76b37
dpi0 <dpi0.dev@proton.me>
2024-09-23 13:25:37 +0530

    feat: add link and page context & menu icons

--- background.js
+++ background.js
@@ -1,18 +1,15 @@
 let previousTabId = null;
 
-// Function to handle copying the current page's URL to the clipboard
-function copyUrl() {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const url = tabs[0].url;
-    navigator.clipboard
-      .writeText(url)
-      .then(() => {
-        console.log("URL copied to clipboard:", url);
-      })
-      .catch((error) => {
-        console.error("Failed to copy URL to clipboard:", error);
-      });
-  });
+// Function to handle copying the URL to the clipboard
+function copyUrl(url) {
+  navigator.clipboard
+    .writeText(url)
+    .then(() => {
+      console.log("URL copied to clipboard:", url);
+    })
+    .catch((error) => {
+      console.error("Failed to copy URL to clipboard:", error);
+    });
 }
 
 // Function to switch to the previously active tab
@@ -44,10 +41,13 @@ function switchTabs() {
 }
 
 // Function to remove playlist part from YouTube URLs and open the video in a new tab
-function removePlaylist(tab) {
-  const url = new URL(tab.url);
-  if (url.hostname === "www.youtube.com" && url.searchParams.has("v")) {
-    const videoId = url.searchParams.get("v");
+function removePlaylist(url) {
+  const parsedUrl = new URL(url);
+  if (
+    parsedUrl.hostname === "www.youtube.com" &&
+    parsedUrl.searchParams.has("v")
+  ) {
+    const videoId = parsedUrl.searchParams.get("v");
     const newUrl = `https://www.youtube.com/watch?v=${videoId}`;
     chrome.tabs.create({ url: newUrl });
   }
@@ -56,34 +56,50 @@ function removePlaylist(tab) {
 // Function to handle context menu click
 function handleContextMenuClick(info, tab) {
   if (info.menuItemId === "copyCurrentUrl") {
-    copyUrl();
+    if (info.linkUrl) {
+      copyUrl(info.linkUrl);
+    } else {
+      copyUrl(tab.url);
+    }
   } else if (info.menuItemId === "removePlaylist") {
-    removePlaylist(tab);
+    if (info.linkUrl) {
+      removePlaylist(info.linkUrl);
+    } else {
+      removePlaylist(tab.url);
+    }
   }
 }
 
 // Create the context menu items
-chrome.contextMenus.create({
+browser.contextMenus.create({
   id: "copyCurrentUrl",
-  title: "Copy Current URL",
-  contexts: ["all"],
+  title: "Copy URL",
+  contexts: ["link", "page"],
+  icons: {
+    48: `icons/copy-url-48.svg`,
+  },
 });
 
-chrome.contextMenus.create({
+browser.contextMenus.create({
   id: "removePlaylist",
-  title: "Open in New Tab without Playlist",
-  contexts: ["all"],
+  title: "Open YouTube Video in New Tab without Playlist",
+  contexts: ["link"],
+  icons: {
+    48: `icons/no-playlist-48.svg`,
+  },
 });
 
 // Listen for commands
 chrome.commands.onCommand.addListener((command) => {
   if (command === "copy-url") {
-    copyUrl();
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      copyUrl(tabs[0].url);
+    });
   } else if (command === "switch-tabs") {
     switchTabs();
   } else if (command === "remove-playlist") {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      removePlaylist(tabs[0]);
+      removePlaylist(tabs[0].url);
     });
   }
 });
